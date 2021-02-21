31054eeca5eb4d5b542977eacca39a60e9643be4
Ming <mingyli34@gmail.com>
2021-02-21 13:08:59 -0800

    Add .gitignore Cargo.toml

---
+++ README.md
@@ -0,0 +1,37 @@
+# porco
+
+Composable probability distributions.
+
+## Examples
+
+Create simple probability distributions.
+
+```rust
+enum Coin {
+    Heads,
+    Tails,
+}
+ 
+impl Coin {
+    fn flip() -> Distribution<Coin> {
+        Distribution::uniform([Coin::Heads, Coin::Tails])
+    }
+}
+ 
+let coin = Coin::flip();
+assert_eq!(coin.pmf(&Coin::Heads), Probability(0.5));
+```
+
+Compose operations over distributions using combinators.
+
+```rust
+fn reflip_if_tails(coin: Coin) -> Distribution<Coin> {
+    match coin {
+        Coin::Heads => Distribution::always(Coin::Heads),
+        Coin::Tails => Coin::flip(),
+    }
+}
+ 
+let coin = Coin::flip().and_then(reflip_if_tails);
+assert_eq!(coin.pmf(&Coin::Heads), Probability(0.75));
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,34 @@
 //! Porco is a library for working with and composing probability
 //! distributions.
-//!
+//! 
 //! The API is inspired by the contents of
 //! [Probabilistic Functional Programming in Haskell][paper]
 //! but with naming conventions that match those of [`Option`] and [`Result`]
 //! (such as [`Option::and_then`]).
-//!
-//! ```
+//! 
+//! ```rust
 //! # use porco::{Probability, Distribution};
 //! # #[derive(Debug, PartialEq, Eq)]
 //! enum Coin {
 //!     Heads,
 //!     Tails,
 //! }
-//!
+//! 
 //! impl Coin {
 //!     fn flip() -> Distribution<Coin> {
 //!         Distribution::uniform([Coin::Heads, Coin::Tails])
 //!     }
 //! }
-//!
+//! 
 //! let coin = Coin::flip();
 //! assert_eq!(coin.pmf(&Coin::Heads), Probability(0.5));
 //! ```
-//!
+//! 
 //! You can compose various operations over `Distribution`s using combinators
 //! like [`Distribution::map`], [`Distribution::and_then`], and
 //! [`Distribution::given`].
-//!
-//! ```
+//! 
+//! ```rust
 //! # use porco::{Probability, Distribution};
 //! # #[derive(Debug, PartialEq, Eq)]
 //! # enum Coin {
@@ -47,10 +47,11 @@
 //!         Coin::Tails => Coin::flip(),
 //!     }
 //! }
-//!
-//! let face = Coin::flip().and_then(reflip_if_tails);
-//! assert_eq!(face.pmf(&Coin::Heads), Probability(0.75));
+//! 
+//! let coin = Coin::flip().and_then(reflip_if_tails);
+//! assert_eq!(coin.pmf(&Coin::Heads), Probability(0.75));
 //! ```
+//! 
 //! [paper]: https://web.engr.oregonstate.edu/~erwig/papers/PFP_JFP06.pdf
 mod assoc_list;
 mod dist;
